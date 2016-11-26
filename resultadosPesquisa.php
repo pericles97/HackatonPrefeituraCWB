79338f5d644611bcdf00d961255162086ec69216
Eduardo Guedes <eduardo.s.guedes@hotmail.com>
2016-11-26 20:13:06 -0200

    Inclusão de pagina de resultado de pesquisa.

---
+++ resultadosPesquisa.php
@@ -0,0 +1,84 @@
+<?php include('menu.php');?>
+<div class="row wrapper border-bottom white-bg page-heading">
+                <div class="col-lg-9">
+                    <h2>Pagina de Pesquisa</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="index.html">Material</a></li>
+                        <li>Professor (800 materiais / 310 Professores)</li>
+                    </ol>
+                </div>
+            </div>
+        <div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-content">
+                            <h2>
+                                1110 Resultados foram encontrados com o tema: <span class="text-navy">“Biologia”</span>
+                            </h2>
+                            <p><small>Tempo de pesquisa</small><small> (0.23 segundos)</small>                            </p>
+                            <div class="search-form">
+                                <form action="index.html" method="get">
+                                    <div class="input-group">
+                                        <input type="text" placeholder="Admin Theme" name="search" class="form-control input-lg">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-lg btn-primary" type="submit">
+                                                Search
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                                <h3><a href="#">Definição de Biologia</a></h3>
+                                <p><em>Biologia</em> (do grego βιος - bios = vida e λογος - logos = estudo, ou seja, o estudo da vida) é a ciência que estuda os seres vivos. Debruça a origem, evolução e o ...</p>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                                <h3><a href="http://www.sobiologia.com.br/conteudos/Biologia/" onmousedown="return rwt(this,'','','','5','AFQjCNFweidswb-YpEDO9KUJ1Lnvtc8BhA','DHkOl4Vg25R9kCWIlyU07A','0ahUKEwiAoNaytcfQAhUCIZAKHZGdBwUQFgg_MAQ','','',event)">Biologia - Ensino Médio - Conteúdos - Só Biologia</a><a href="#"></a></h3>
+                                <p>Esta seção é dedicada ao ensino de <em>Biologia</em> (conteúdos de ensino médio). Clique sobre o nome do assunto desejado para visualizar o seu conteúdo.</p>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                              <h3><a href="http://site.biologiatotal.com.br/" onmousedown="return rwt(this,'','','','8','AFQjCNE5TFzeGc_EUMgs7vf3Vxs8xqJaeQ','rxgTomj2f0aj1CHNxQCakw','0ahUKEwiAoNaytcfQAhUCIZAKHZGdBwUQFghWMAc','','',event)">Biologia Total – ENEM, Vestibulares e Faculdades | Prof. Jubilut</a><a href="https://www.biologiatotal.com.br/areas-da-biologia/" onmousedown="return rwt(this,'','','','7','AFQjCNEa73hYqGa4icwx-EKu3FiJILe2WA','gTkSm2nDMpQhmbiy1BsNMg','0ahUKEwiAoNaytcfQAhUCIZAKHZGdBwUQFghQMAY','','',event)"></a><a href="#"></a></h3>
+                                <p>Uma nova experiência de aprendizado. A melhor plataforma de <em>biologia</em> do Brasil. Jubi, quero manjar <em>biologia</em>. Entenda como funciona o <em>Biologia</em> Total ...
+                                </p>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                                <h3><a href="https://mesalva.com/cursos/biologia" onmousedown="return rwt(this,'','','','10','AFQjCNHIPAXjqGLZySb2h1Zz1UAxfxMPVQ','gXogQFmCie2DePB0sSqlIw','0ahUKEwiAoNaytcfQAhUCIZAKHZGdBwUQFghgMAk','','',event)">Biologia - Me Salva!</a><a href="#"></a></h3>
+                                <p>Extensivo de <em>Biologia</em>. Rogério Fernandes Sophie Maillard von Eye Andressa Helrighel Vivian Bamberg Corassini Elisa Negri. 21 módulos 386 aulas 460 .
+                              </p>
+                          </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                                <h3><a href="http://www.todabiologia.com/" onmousedown="return rwt(this,'','','','12','AFQjCNFiaR1r21ExMY03p06-v0IfQdTn4w','4CKBof5NzhNLWS6gQ6L2ZQ','0ahUKEwiAoNaytcfQAhUCIZAKHZGdBwUQFghtMAs','','',event)">Toda Biologia - Ciências Biológicas, resumos de Biologia</a><a href="#"></a></h3>
+                                <p><em>Biologia</em>, Anatomia, Citologia, <em>Biologia</em> Celular, Saúde, Zoologia, Botânica, Microbiologia, Genética, Ecologia, Resumos de <em>Biologia</em>.</p>
+                            </div>
+                            <div class="hr-line-dashed"></div>
+                            <div class="search-result">
+                                <h3><a href="https://www.ib.unicamp.br/" onmousedown="return rwt(this,'','','','15','AFQjCNFoWy130CPCXTW6fa6dmzW6UMsd8g','GfB14tK9EsDnFiz_Ra7oJA','0ahUKEwjizqbDtsfQAhUHh5AKHXnMCIs4ChAWCDwwBA','','',event)">Instituto de Biologia | Instituto de Biologia</a><a href="#"></a></h3>
+                                <p>Informações sobre cursos de graduação e pós-graduação, projetos e laboratórios de pesquisa. Universidade Estadual de Campinas, SP, Brasil.</p>
+                            </div><div class="text-center"><div class="btn-group">
+                                    <button class="btn btn-white" type="button"><i class="fa fa-chevron-left"></i></button>
+                                    <button class="btn btn-white">1</button>
+                                    <button class="btn btn-white  active">2</button>
+                                    <button class="btn btn-white">3</button>
+                                    <button class="btn btn-white">4</button>
+                                    <button class="btn btn-white">5</button>
+                                    <button class="btn btn-white">6</button>
+                                    <button class="btn btn-white">7</button>
+                                    <button class="btn btn-white" type="button"><i class="fa fa-chevron-right"></i> </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </div>
+        
+
+<?php include('footer.php');?>
