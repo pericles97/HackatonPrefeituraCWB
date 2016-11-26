c4db54ee14cbab31593df9eea80ea1940e96848d
Eduardo Guedes <eduardo.s.guedes@hotmail.com>
2016-11-26 09:10:21 -0200

    Teste integrando HTML com PHP.

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+<?php
+$valor1 = 12;
+$valor2 = 13;
+$valor3 = $valor1 + $valor2;
+
+?>
+
 <html>
 <b>Hello World</b>
 <h1>Teste3</h1>
 <h1>Partiu Aprender Galera</h1>
+<h1> <?= $valor3?> </h1>
 </html>
