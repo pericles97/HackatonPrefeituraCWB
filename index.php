21487c920bc8bec74e9888faa0882f24355f4f36
Eduardo Guedes <eduardo.s.guedes@hotmail.com>
2016-11-27 00:17:41 -0200

    teste3

--- index.php
+++ index.php
@@ -29,6 +29,17 @@
                 <small>
                     Portal de compartilhamento de conteudo on-line para aulas.
                 </small>
+				<h1><?php = $tt['Sobrenome']?><h1>
+ <h1><?php = $tt['Endereco']?><h1>
+ <h1><?php = $tt['Estado']?><h1>
+ <h1><?php = $tt['Cidade']?><h1>
+ <h1><?php = $tt['Telefone']?><h1>
+ <h1><?php = $tt['Email']?><h1>
+ <h1><?php = $tt['AreaAtuacao']?><h1>
+ <h1><?php = $tt['Nivel']?><h1>
+ <h1><?php = $tt['Senha']?><h1>
+ <h1><?php = $tt['FaleUmPoucoSobreVoce']?><h1>
+ <h1><?php =  "Teste conxcao"?><h1>
             </div>
 			<div class="text-center m-t-lg">
 				<img alt="image" class="img-circle" src="img/teach_hub_logo cortada.png" />
